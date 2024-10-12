674d6bea7a8e45b3216911f7daa5ae6e34ca0843
Samuel von Baussnern <samuel.vonbaussnern@d-one.ai>
2024-10-12 10:13:33 +0200

    fixes timestamp of solution

--- src/test/java/org/done/flink/exercises/util/cep/Ex03.java
+++ src/test/java/org/done/flink/exercises/util/cep/Ex03.java
@@ -16,7 +16,7 @@ public class Ex03 {
 
     public static List<Event> getExpectedOutput() {
         return List.of(
-                new Event("339966b51f42510ca487d3f9609483b4", EventType.PERSON_HAS_ENTERED_THE_ROOM, Instant.parse("2024-09-05T14:00:47Z")),
+                new Event("339966b51f42510ca487d3f9609483b4", EventType.PERSON_HAS_ENTERED_THE_ROOM, Instant.parse("2024-09-05T13:56:46Z")),
                 new Event("339966b51f42510ca487d3f9609483b4", EventType.WINDOW_OPENED, Instant.parse("2024-09-05T14:20:53Z"))
         );
     }
